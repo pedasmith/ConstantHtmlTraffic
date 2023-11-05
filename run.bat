970caa1aaa942e44ed0ed928538198535c797d99
Peter Smith <pesmith@microsoft.com>
2023-11-05 15:09:59 -0800

    automatically set title and color for IIS Express

--- run.bat
+++ run.bat
@@ -5,4 +5,6 @@ set WEBAPP="C:\HtmlApps\ConstantHTMLTraffic"
 REM Original command: %IISEXPRESS% /trace:info /path:%WEBAPP%
 REM CHT stands for ConstantHtmlTraffic
 REM /trace:info actually causes slowdowns in the stats!
+color 5f
+title IIS Express -- press Q to quit
 %IISEXPRESS% /trace:error /config:ConstantHTMLTraffic.config /site:CHT > delete_me.log
